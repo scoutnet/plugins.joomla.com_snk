3d7f822f6603efab93fe002b9bf4da2c61725bf9
Stefan Horst <stefan@ultrachaos.de>
2009-09-21 17:18:32 +0000

    updated some template

--- site/views/snk/view.html.php
+++ site/views/snk/view.html.php
@@ -1,20 +1,19 @@
 <?php
 jimport( 'joomla.application.component.view');
 
-class SnkViewSnk extends JView
-{
-	function display($tpl = null)
-	{
-		$events = $this->get( 'Events' );
-		$kalenders = $this->get('Kalenders');
-		//$params = &JComponentHelper::getParams( 'com_snk' );
-		//$greeting =  $params->get( 'SSID' );
+class SnkViewSnk extends JView {
+	function display($tpl = null) {
+		try {
+			$events = $this->get( 'Events' );
+			$kalenders = $this->get('Kalenders');
 
+			$this->assignRef( 'events', $events );
+			$this->assignRef( 'kalenders', $kalenders );
 
-		$this->assignRef( 'events', $events );
-		$this->assignRef( 'kalenders', $kalenders );
-
-		parent::display($tpl);
+			parent::display($tpl);
+		} catch (Exception $e) {
+			echo "<span class='termin'>zZ ist der Scoutnet Kalender down.<br>Bitte versuch es zu einem sp&auml;teren Zeitpunkt noch mal</span>";
+		}
 	}
 }
 ?>
