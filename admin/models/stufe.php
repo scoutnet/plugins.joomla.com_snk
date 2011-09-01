7b5338ea38920a36536f84ce33ba1660a4558bae
Stefan Horst <stefan@ultrachaos.de>
2011-09-01 11:08:08 +0000

    added models

---
+++ admin/models/stufe.php
@@ -0,0 +1,16 @@
+<?php
+class stufe extends ArrayObject{
+	function __construct( $array ){
+		parent::__construct($array);
+	}
+
+
+	public function get_Image_URL(){
+		if (isset($this['id'])) {
+			return (string) "<img src='http://kalender.scoutnet.de/2.0/images/".$this['id'].".gif' alt='".htmlentities($this['bezeichnung'])."' />";
+		} 
+
+		return (string) "";
+	}
+
+}
