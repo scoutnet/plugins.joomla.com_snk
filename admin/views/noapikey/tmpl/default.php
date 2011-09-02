ecb737bdcd3e1863f586974ded0bfe36ac9b44aa
Stefan Horst <stefan@ultrachaos.de>
2011-09-02 07:51:54 +0000

    changed to correct url

--- admin/views/noapikey/tmpl/default.php
+++ admin/views/noapikey/tmpl/default.php
@@ -8,7 +8,7 @@ JHtml::_('behavior.tooltip');
 <?php 
 try {
 	$text = "Your account is not connected to any Scoutnet User yet. Please use the Login Button to connect your Scoutnet User to this Joomla user.";
-	$text .= $this->sn->get_scoutnetConnectLoginButton('https://www.dpsg-erft.de/joomla/administrator/index.php?option=com_snk',true); 
+	$text .= $this->sn->get_scoutnetConnectLoginButton(JURI::base().'index.php?option=com_snk',true); 
 	echo $text;
 } catch (com_snk_sn_Exception_MissingConfVar $e) {
  echo $e->getMessage();
