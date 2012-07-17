e22c8436d4411d0075bb1efd7aeef09b50521767
stefan <stefan@ultrachaos.de>
2012-07-17 21:09:53 +0200

    removed old files

--- component/admin/models/forms/event.js
+++
@@ -1,7 +0,0 @@
-window.addEvent('domready', function() {
-		document.formvalidator.setHandler('time',
-			function (value) {
-			regex=/^(([01]|)[0-9]|2[0-4]):[0-5][0-9]$/;
-			return regex.test(value);
-			});
-		});
