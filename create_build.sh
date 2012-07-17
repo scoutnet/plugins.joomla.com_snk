103265710436e3896be9d31829fca4233f6b14f3
stefan <stefan@ultrachaos.de>
2012-07-17 22:12:34 +0200

    changed create build file to tag commit and create update script for all tags

--- create_build.sh
+++ create_build.sh
@@ -8,27 +8,26 @@ if [ ! -e build ]; then
 	mkdir build
 fi
 
-
 if [ ! -e build/$name-$version-final.zip ]; then
 	cd src
 	zip -r $name-$version-final.zip *
 	mv $name-$version-final.zip ../build/
 	cd ..
 
-	# TODO: create TAG 
-	# git add build/$name-$version-final.zip
 	git tag -a $version -m "version $version"
 
-	xml=$(cat build/${name}_update.xml | grep -v "</updates>")
+	versions=(${=$(git tag)})
 
-	echo $xml > build/${name}_update.xml
+	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<updates>" > build/${name}_update.xml
 
-	for joomla_version in '1.6' '1.7' '2.5'; do
-		for client_id in '' '<client_id>1<\/client_id>'; do
+	for update_version in $versions; do
+		for joomla_version in '1.6' '1.7' '2.5'; do
+			for client_id in '' '<client_id>1<\/client_id>'; do
 
-			xml=$(cat update_template.xml | sed "s/###NAME###/$name/g" | sed "s/###VERSION###/$version/g" | sed "s/###CLIENT_ID###/$client_id/g" | sed "s/###JOOMLA_VERSION###/$joomla_version/g")
+				xml=$(cat update_template.xml | sed "s/###NAME###/$name/g" | sed "s/###VERSION###/$update_version/g" | sed "s/###CLIENT_ID###/$client_id/g" | sed "s/###JOOMLA_VERSION###/$joomla_version/g")
 
-			echo $xml >> build/${name}_update.xml
+				echo $xml >> build/${name}_update.xml
+			done
 		done
 	done
 
@@ -50,5 +49,5 @@ if [ ! -e build/$name-$version-final.zip ]; then
 
 	#svn add scoutnet_download/${name}-$version-final.zip
 	#svn commit -m "new Version for ${name} $version" scoutnet_download
-fi
 
+fi
